621a9ab48aeb3efd407cbc6aa3777f0dd36717e2
Jeman Ryu <thekrjm@naver.com>
2024-08-28 16:12:44 +0900

    banner

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Banner.styles.css'
 import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies';
 import Alert from 'react-bootstrap/Alert';
 
 const Banner = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
   console.log('data', data);
+  console.log('errorrrrr',error);
+  
 
   if (isLoading) {
     <h1>Loading......</h1>;
@@ -14,7 +17,17 @@ const Banner = () => {
     <Alert variant='danger'>{error.message}</Alert>;
   }
 
-  return <div>Banner</div>;
+  return <div 
+  style={{
+    backgroundImage:`url("https://media.themoviedb.org/t/p/w533_and_h300_bestv2/${data?.results[0].backdrop_path}")`,
+  }}
+  className='banner'
+  >
+    <div>
+      <h1>{data?.results[0].title}</h1>
+      <div>{data?.results[0].overview}</div>
+    </div>
+  </div>;
 };
 
 export default Banner;
